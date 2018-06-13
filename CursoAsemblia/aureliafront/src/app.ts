084d03d97cfcbcee37b4338d2559561f8b36ea0e
kintela <roberto.quintela@gmail.com>
2018-06-13 11:36:02 +0200

    Accedes al elemento h1 en attached para poner el h1 en cursiva

--- CursoAsemblia/aureliafront/src/app.ts
+++ CursoAsemblia/aureliafront/src/app.ts
@@ -2,6 +2,9 @@ export class App {
   welcomeTitle='Bienvenido a Proyectos';
 
   constructor(){
+  }
+
+  attached(){
     var h1=document.getElementsByTagName('h1');
     h1[0].style.fontStyle='italic';
   }

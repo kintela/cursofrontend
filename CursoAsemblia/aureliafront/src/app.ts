553c65574d3a97915162441270e3360e4212a6a6
kintela <roberto.quintela@gmail.com>
2018-06-13 11:40:35 +0200

    databinding de una via para interpolar la hora

--- CursoAsemblia/aureliafront/src/app.ts
+++ CursoAsemblia/aureliafront/src/app.ts
@@ -7,5 +7,9 @@ export class App {
   attached(){
     var h1=document.getElementsByTagName('h1');
     h1[0].style.fontStyle='italic';
+
+    window.setInterval(()=>{
+      this.welcomeTitle=`Bienvenido a proyectos. Son las :${new Date().toString()}`;
+    },1000)
   }
 }

f31cb361b96a3565aa8c6ab81fc0aa93bd17b2a9
Kintela <roberto.quintela@gmail.com>
2018-06-21 09:31:21 +0200

    Meter los VakueConverters como recursos glogales

--- CursoAsemblia/aureliafront/src/resources/index.ts
+++ CursoAsemblia/aureliafront/src/resources/index.ts
@@ -4,6 +4,8 @@ import { FrameworkConfiguration } from "aurelia-framework";
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
     PLATFORM.moduleName("./elements/message"),
-    PLATFORM.moduleName("./elements/navBar")
+    PLATFORM.moduleName("./elements/navBar"),
+    PLATFORM.moduleName("./value-converters/formatDateValueConverter"),
+    PLATFORM.moduleName("./value-converters/formatNumberValueConverter")
   ]);
 }

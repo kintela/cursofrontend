4e566510e3eece386c271a0f59f966b805221759
Kintela <roberto.quintela@gmail.com>
2018-06-21 09:38:07 +0200

    Crear propiedad calculada para unir 2 campos en el modelo

--- CursoAsemblia/aureliafront/src/resources/proyecto.ts
+++ CursoAsemblia/aureliafront/src/resources/proyecto.ts
@@ -1,21 +1,22 @@
-import * as moment from 'moment';
-import * as numeral from 'numeral';
-import { Moment } from 'moment';
+import * as moment from "moment";
+import * as numeral from "numeral";
+import { Moment } from "moment";
 
-export class Proyecto{
-  constructor(json:any){
-    if(!json){
+export class Proyecto {
+  constructor(json: any) {
+    if (!json) {
       return;
     }
 
-    Object.assign(this,json,{
-      workStartDate:moment(json.workStartDate,'YYYY-MM-DD'),
-      tenderBudget:numeral(json.tenderBudget)
+    Object.assign(this, json, {
+      workStartDate: moment(json.workStartDate, "YYYY-MM-DD"),
+      tenderBudget: numeral(json.tenderBudget),
+      customerCodePlusId: json.customerCode + json.id
     });
   }
-  id:string;
-  name:string;
-  workStartDate:Moment;
-  customerCode:string;
-  tenderBudget:Numeral;
+  id: string;
+  name: string;
+  workStartDate: Moment;
+  customerCode: string;
+  tenderBudget: Numeral;
 }
